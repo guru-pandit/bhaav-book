75338038004234eab863d0d25bb2b0b57781f617
Guruprasad Pandit <gppandit.gp@gmail.com>
2026-08-08 17:07:27 +0530

    Update ProductListScreen.kt

--- app/src/main/java/com/bhaavbook/app/ui/screens/ProductListScreen.kt
+++ app/src/main/java/com/bhaavbook/app/ui/screens/ProductListScreen.kt
@@ -86,6 +86,7 @@ import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
